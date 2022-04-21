56201e44eeb1825777c8aff45d0b203baa7bdd47
madicasta <mcorreac@ucsc.edu>
2022-04-20 22:09:41 -0700

    added lab 4-5 including test of artist page

---
+++ lab4/js/lab.js
@@ -0,0 +1,38 @@
+//* Summary. (use period)
+// * Description. (use period)
+// * @link   URL
+// * @file   This files defines the MyClass class.
+// * @author AuthorName.
+// * @since  x.x.x
+
+// Creating a Hang-man game:
+// 1. Print hang-man base template before each turn
+// 2. Generate 1 phrase of 5
+// 3. Accept user input
+// 4. Validate user move
+// 5. Evaluate possible values
+// 6. Accept correct values
+// 7. Print single shape if value input is correct
+// 8. Test win or loss conditions
+
+// Print the board at the start of the game
+    // Add shape after each incorrect letter
+        // End game if body is complete before variables are filled
+
+
+
+// Creating a Tic-Tac-Toe game:
+// 1. Print the board before each turn
+// 2. Accept user input
+// 3. Validate user move
+// 4. Evaluate possible moves
+// 5. Choose computer move
+// 6. Test of win or draw condition
+
+// Print the board before each turn
+    // Loop through each row
+        // Loop through each space
+            // Print X, O, or Space
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Madisen Castaneda <mcorreac@ucsc.edu>
+ * Created 20 April 2022
+ * License: Public Domain
+ */
+
+// Define variables
+make = "Honda";
+model = "Accord";
+color = "White";
+year = 2019;
+
+// calculate
+age = 2019 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
